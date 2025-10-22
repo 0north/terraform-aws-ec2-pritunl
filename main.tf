85623e2cf7bfec78876d8c9bc6f781685d98667d
Brian H. Skafte <brian.skafte@zeronorth.com>
2025-10-22 07:48:17 +0200

    remove / prefix from ssm_path_* in ssm:PutParameter policy arns

--- main.tf
+++ main.tf
@@ -311,16 +311,16 @@ resource "aws_iam_policy" "ssm_put_parameter" {
         Effect = "Allow",
         Action = ["ssm:PutParameter"]
         Resource = [
-          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${local.ssm_path_default_credentials}",
-          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${local.ssm_path_key_pair}"
+          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${trimprefix(local.ssm_path_default_credentials, "/")}",
+          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${trimprefix(local.ssm_path_key_pair, "/")}"
         ]
       },
       {
         Effect = "Deny",
         Action = ["ssm:PutParameter"],
         Resource = [
-          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${local.ssm_path_default_credentials}",
-          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${local.ssm_path_key_pair}"
+          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${trimprefix(local.ssm_path_default_credentials, "/")}",
+          "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${trimprefix(local.ssm_path_key_pair, "/")}"
         ],
         Condition = {
           "StringEquals" = {
